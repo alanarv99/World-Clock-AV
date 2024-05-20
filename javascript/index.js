c0c053730561c6fe064cac2f78abcec9d81b5091
alanarv99 <147183094+alanarv99@users.noreply.github.com>
2024-05-19 22:48:24 -0400

    added current location option to dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -43,9 +43,13 @@ function updateTime() {
   }
 }
 function updateCityDropdown(event) {
-  let cityTimezone = event.target.value;
-  let cityName = cityTimezone.replace("_", " ").split("/")[1];
-  let cityTime = moment.tz(cityTimezone);
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment.tz(cityTimeZone);
   console.log(cityTime.format("MMMM Do YYYY"));
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = ` <div class="city">
